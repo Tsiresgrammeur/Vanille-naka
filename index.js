ff58654c75400a3837d6e584cf4d44218b7d69a0
Tsiresgrammeur <tsiresgrammeur@gmail.com>
2022-01-19 08:39:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router =require('./routes');
+const cors=require('cors');
+const app = express();
+app.use(express.urlencoded({limit:'20MB',extended:true,parameterLimit:20000}));
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.use('/image',express.static('image'))
+
+app.listen(8080, () => console.log('app listening at 8080'));
